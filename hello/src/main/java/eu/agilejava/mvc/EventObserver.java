f4dd88be07b56ad623f5df4b484e1682308a8517
Ivar Grimstad <ivar.grimstad@gmail.com>
2016-06-11 08:43:44 +0100

    Add event observers

---
+++ hello/src/main/java/eu/agilejava/mvc/EventObserver.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Ivar Grimstad (ivar.grimstad@gmail.com).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package eu.agilejava.mvc;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.mvc.event.AfterControllerEvent;
+import javax.mvc.event.AfterProcessViewEvent;
+import javax.mvc.event.BeforeControllerEvent;
+import javax.mvc.event.BeforeProcessViewEvent;
+import javax.mvc.event.ControllerRedirectEvent;
+
+/**
+ *
+ * @author Ivar Grimstad (ivar.grimstad@gmail.com)
+ */
+@ApplicationScoped
+public class EventObserver {
+
+    public void onBeforeController(@Observes BeforeControllerEvent event) {
+        System.out.println("Before Controller");
+    }
+
+    public void onAfterController(@Observes AfterControllerEvent event) {
+        System.out.println("After Controller");
+    }
+
+    public void onBeforeView(@Observes BeforeProcessViewEvent event) {
+        System.out.println("Before View");
+    }
+
+    public void onAfterView(@Observes AfterProcessViewEvent event) {
+        System.out.println("After View");
+    }
+
+    public void onRedirect(@Observes ControllerRedirectEvent event) {
+        System.out.println("Redirect");
+    }
+}
